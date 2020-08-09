2c299cb3dbc6c4057c19bbaa8e7e89ddb805c8c9
L1STOV <listovspartak99@gmail.com>
2020-08-09 16:34:09 +0300

    Re-search every 5 minutes

--- autoria.py
+++ autoria.py
@@ -1,44 +1,47 @@
 from bs4 import BeautifulSoup
 import requests
 from car import Car
-
-url = "https://auto.ria.com/legkovie/bmw/325/?page="
-
-count = 0
-list_of_parsed_cars = []
-
-
-def parse_cars(items):
-    local_list_of_parsed_cars = []
-    for item in items:
-        car_object = Car()
-        a_element = item.find('div', class_='content').find('a')
-        car_object.url = a_element['href']
-        car_object.title = a_element['title']
-        local_list_of_parsed_cars.append(car_object)
-    return local_list_of_parsed_cars
-
+import time
 
 while True:
-    file = open("File.txt", "w", encoding="utf-8")
-    print('Parsing ' + str(count) + ' page')
-    response = requests.request("GET", url + str(count))
-    soup = BeautifulSoup(response.text, 'html.parser')
-    items = soup.find_all('section', class_="ticket-item")
-    parsed_cars = parse_cars(items)
-    if len(parsed_cars) == 0:
-        break
-    for car in parsed_cars:
-        list_of_parsed_cars.append(car)
-    count += 1
+    url = "https://auto.ria.com/legkovie/bmw/325/?page="
 
-for car in list_of_parsed_cars:
-    print(car.url)
-    print(car.title)
-    print('\n')
-    file.write(car.url + '\n')
-file.close()
+    count = 0
+    list_of_parsed_cars = []
 
-print('Parsing finished! ')
-print('Found ' + str(len(list_of_parsed_cars)) + ' cars')
 
+    def parse_cars(items):
+        local_list_of_parsed_cars = []
+        for item in items:
+            car_object = Car()
+            a_element = item.find('div', class_='content').find('a')
+            car_object.url = a_element['href']
+            car_object.title = a_element['title']
+            local_list_of_parsed_cars.append(car_object)
+        return local_list_of_parsed_cars
+
+
+    while True:
+        file = open("File.txt", "w", encoding="utf-8")
+        print('Parsing ' + str(count) + ' page')
+        response = requests.request("GET", url + str(count))
+        soup = BeautifulSoup(response.text, 'html.parser')
+        items = soup.find_all('section', class_="ticket-item")
+        parsed_cars = parse_cars(items)
+        if len(parsed_cars) == 0:
+            break
+        for car in parsed_cars:
+            list_of_parsed_cars.append(car)
+        count += 1
+
+    for car in list_of_parsed_cars:
+        print(car.url)
+        print(car.title)
+        print('\n')
+        file.write(car.url + '\n')
+    file.close()
+
+    print('Parsing finished! ')
+    print('Found ' + str(len(list_of_parsed_cars)) + ' cars')
+
+    time.sleep(300)
